02a85502f185bec85c5364675de27634f40c3914
xieli <2524546110@qq.com>
2020-12-02 18:56:56 +0800

    nowcoder

--- src/牛客题霸/数组/买卖股票的最佳时机NC7.java
+++ src/牛客题霸/数组/买卖股票的最佳时机NC7.java
@@ -3,5 +3,30 @@ package 牛客题霸.数组;
 /**
  * Created by xieli on 2020/11/30.
  */
+
+
 public class 买卖股票的最佳时机NC7 {
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (prices == null || n == 0)
+            return 0;
+
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                // 此处必须注意要跳过这次判定
+                continue;
+            }
+
+            // 注意！！！推导状态方程一定要加上交易次数 k 的限制
+            dp[i][0] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][0]);
+            dp[i][1] = Math.max(-prices[i], dp[i - 1][1]);
+        }
+
+        return dp[n - 1][0];
+    }
+
 }
